c0812b04c361f7dc96d9a02dc574dfecd244092f
Devin Balkind <devinbalkind@gmail.com>
2025-12-26 21:40:45 -0500

    Install and configure Strapi Preview Button plugin

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,1 +1,22 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    'preview-button': {
+        config: {
+            contentTypes: [
+                {
+                    uid: 'api::page.page',
+                    published: {
+                        url: 'https://wegov.nyc/{slug}',
+                    },
+                    draft: {
+                        url: 'https://wegov.nyc/api/preview',
+                        query: {
+                            type: 'api::page.page',
+                            slug: '{slug}',
+                            secret: env('STRAPI_PREVIEW_SECRET', 'wegov-preview-secret'),
+                        },
+                    },
+                },
+            ],
+        },
+    },
+});
